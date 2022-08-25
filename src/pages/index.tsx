a718e42b77a9639fc81999ec2fc27feb0010e46e
unknown <hi@devcomp.tk>
2022-08-25 09:49:56 +0530

    feat: :eyes:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,36 +4,38 @@ import DashboardExample from "../components/DashboardExample";
 import styles from "../styles/pages/home.module.css";
 const Home: NextPage = () => {
   return (
-    <div className={styles.home}>
-      <div className={styles.left}>
-        <DashboardExample />
-        <div className={styles.info}>
-          <h1>Keep track of everything.</h1>
-          <p>
-            Everything from simple player statistics, to custom events and feature flags. All in one place.
-          </p>
+    <body>
+      <script>console.log("Hello there, fellow adventurer! 📣🧠")</script>
+      <div className={styles.home}>
+        <div className={styles.left}>
+          <DashboardExample />
+          <div className={styles.info}>
+            <h1>Keep track of everything.</h1>
+            <p>
+              Everything from simple player statistics, to custom events and feature flags. All in one place.
+            </p>
+          </div>
+          <div className={styles.info}>
+            <h1>Customizability to it&#39;s max.</h1>
+            <p>
+              Want something that isn&#39;t supported already? Want to add your own custom feature? Datalink supports it.
+            </p>
+          </div>
+          <div className={styles.info}>
+            <h1>Easy to implement.</h1>
+            <p>
+              Simply add a module script to your game, and set your token, you&#39;re good to go!
+            </p>
+          </div>
         </div>
-        <div className={styles.info}>
-          <h1>Customizability to it&#39;s max.</h1>
-          <p>
-            Want something that isn&#39;t supported already? Want to add your own custom feature? Datalink supports it.
-          </p>
-        </div>
-        <div className={styles.info}>
-          <h1>Easy to implement.</h1>
-          <p>
-            Simply add a module script to your game, and set your token, you&#39;re good to go!
-          </p>
-        </div>
-      </div>
-      <div className={styles.right}>
-        <h3>Meet datalink.</h3>
-        <h1>
-          A futuristic and lightweight analytics platform for upcoming ROBLOX
-          games.
-        </h1>
+        <div className={styles.right}>
+          <h3>Meet datalink.</h3>
+          <h1>
+            A futuristic and lightweight analytics platform for upcoming ROBLOX
+            games.
+          </h1>
 
-        {/* <div className={styles.buttons}>
+          {/* <div className={styles.buttons}>
           <Link href="/">
             <a>
               <span>Get Started</span>
@@ -47,8 +49,9 @@ const Home: NextPage = () => {
             </a>
           </Link>
         </div> */}
+        </div>
       </div>
-    </div>
+    </body>
   );
 };
 
