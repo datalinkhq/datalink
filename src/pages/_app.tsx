f2b990f5c0ce28dcbb4c480ed93d0656b5dcbd09
Compey <hi@devcomp.tk>
2022-09-23 19:31:01 +0530

    fix: import `next/head`
    literally how can someone forget to import something 💀

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
