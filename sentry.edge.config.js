19605a3b70c13a94d6ac95724c59fc09032d3733
Compey <hi@devcomp.xyz>
2023-02-06 21:49:56 +0530

    chore: manually 'merge' required commits

---
+++ sentry.edge.config.js
@@ -0,0 +1,33 @@
+// $$$$$$$\             $$\               $$\ $$\           $$\       
+// $$  __$$\            $$ |              $$ |\__|          $$ |      
+// $$ |  $$ | $$$$$$\ $$$$$$\    $$$$$$\  $$ |$$\ $$$$$$$\  $$ |  $$\ 
+// $$ |  $$ | \____$$\\_$$  _|   \____$$\ $$ |$$ |$$  __$$\ $$ | $$  |
+// $$ |  $$ | $$$$$$$ | $$ |     $$$$$$$ |$$ |$$ |$$ |  $$ |$$$$$$  / 
+// $$ |  $$ |$$  __$$ | $$ |$$\ $$  __$$ |$$ |$$ |$$ |  $$ |$$  _$$<  
+// $$$$$$$  |\$$$$$$$ | \$$$$  |\$$$$$$$ |$$ |$$ |$$ |  $$ |$$ | \$$\ 
+// \_______/  \_______|  \____/  \_______|\__|\__|\__|  \__|\__|  \__|     
+                 
+// Copyright (c) 2022 Datalink Contributors. All rights reserved.  
+
+// This source code is licensed under the AGPL license.
+// See LICENSE file in the project root for more details.
+// Original licensing can be found in LICENSE in the root 
+// directory of this source tree.
+
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn: SENTRY_DSN || 'https://859feefdf81f4497b5d921292b88bc64@o1338921.ingest.sentry.io/6610435',
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
