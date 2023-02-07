b006fc2602674c707892fc7697750180140073b7
Marino Franz <mvbjarnasynir@gmail.com>
2023-02-07 22:19:27 +0000

    feat(login): added functionality

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,11 +10,26 @@ import { motion } from "framer-motion";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { SubmitHandler, useForm } from "react-hook-form";
+import axios from "axios";
+import { setCookie } from "nookies";
+
+type FormInput = {
+  username: string;
+  password: string;
+};
 
 const Login: NextPageWithLayout = () => {
   const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInput>();
   const { systemTheme, theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
+  const [mounted, setMounted] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>();
+  const [authenticating, setAuthenticating] = useState<boolean>(false);
 
   useEffect(() => {
     setMounted(true);
@@ -45,6 +60,47 @@ const Login: NextPageWithLayout = () => {
             onClick={() => setTheme("dark")}
           />
         );
+    }
+  };
+
+  const login: SubmitHandler<FormInput> = async (data) => {
+    setAuthenticating(true);
+    setError(null);
+
+    try {
+      const { data: idData } = await axios.post("/api/id", {
+        username: data.username,
+      });
+
+      const { data: iatData } = await axios.post("/api/jwt/iat", {
+        id: idData.id,
+      });
+
+      const { data: signedData } = await axios.post("/api/jwt/sign", {
+        payload: data.password,
+        iat: iatData.iat,
+      });
+
+      const { data: authData } = await axios.post("/api/auth", {
+        id: idData.id,
+        token: signedData.signedToken,
+      });
+
+      setCookie(null, "datalink-id", idData.id, {
+        maxAge: 7 * 24 * 60 * 60,
+        path: "/",
+      });
+
+      setCookie(null, "datalink-session", authData.session_key, {
+        maxAge: 7 * 24 * 60 * 60,
+        path: "/",
+      });
+
+      router.push("/");
+    } catch (error: any) {
+      setAuthenticating(false);
+      if (!error.response.data) return setError("Unexpected error, try again");
+      return setError("Invalid credentials");
     }
   };
 
@@ -111,31 +167,58 @@ const Login: NextPageWithLayout = () => {
                 </span>
                 <div className="flex-grow border-t border-black/20 dark:border-[#D9D9D9]/[18%]" />
               </div>
-              <div className="space-y-[24px]">
+              <form className="space-y-[24px]" onSubmit={handleSubmit(login)}>
                 <label className="block">
                   <span className="text-black/75 dark:text-white/75">
                     Username
                   </span>
                   <input
                     type="text"
-                    className="block mt-1 w-full rounded-xl caret-black dark:caret-white bg-secondary-white dark:bg-secondary border-black/10 dark:border-white/10 placeholder:text-black/[45%] dark:placeholder:text-white/[45%] !outline-[0px] focus:ring-0 focus:border-black/30 dark:focus:border-white/30 transition-all"
+                    className="block mt-1 w-full rounded-xl caret-black dark:caret-white bg-secondary-white dark:bg-secondary border-black/10 dark:border-white/10 placeholder:text-black/[45%] dark:placeholder:text-white/[45%] !outline-[0px] focus:ring-0 focus:border-black/30 dark:focus:border-white/30 transition-all disabled:cursor-not-allowed disabled:opacity-80"
                     placeholder="DevComp"
+                    disabled={authenticating}
+                    {...register("username", { required: true })}
                   ></input>
                 </label>
+                <div className="flex flex-col !mt-2">
+                  {errors.username && (
+                    <span className="text-red-600 font-semibold opacity-100">
+                      This field is required
+                    </span>
+                  )}
+                </div>
                 <label className="block">
                   <span className="text-black/75 dark:text-white/75">
                     Password
                   </span>
                   <input
-                    type="text"
-                    className="block mt-1 w-full rounded-xl caret-black dark:caret-white bg-secondary-white dark:bg-secondary border-black/10 dark:border-white/10 placeholder:text-black/[45%] dark:placeholder:text-white/[45%] placeholder:text-xs !outline-[0px] focus:ring-0 focus:border-black/30 dark:focus:border-white/30 transition-all"
+                    type="password"
+                    className="block mt-1 w-full rounded-xl caret-black dark:caret-white bg-secondary-white dark:bg-secondary border-black/10 dark:border-white/10 placeholder:text-black/[45%] dark:placeholder:text-white/[45%] placeholder:text-xs !outline-[0px] focus:ring-0 focus:border-black/30 dark:focus:border-white/30 transition-all disabled:cursor-not-allowed disabled:opacity-80"
                     placeholder="●●●●●●●●●"
+                    disabled={authenticating}
+                    {...register("password", { required: true })}
                   ></input>
                 </label>
-                <button className="block w-full rounded-xl text-white dark:text-black bg-[#4469A5] dark:bg-[#C3CFE2] font-bold p-2 text-xl hover:scale-[0.98] ease-in-out transition-transform">
+                <div className="flex flex-col !mt-2">
+                  {errors.password && (
+                    <span className="text-red-600 font-semibold opacity-100">
+                      This field is required
+                    </span>
+                  )}
+                  {error && (
+                    <span className="text-red-600 font-semibold opacity-100">
+                      {error}
+                    </span>
+                  )}
+                </div>
+                <button
+                  type="submit"
+                  disabled={authenticating}
+                  className="block w-full rounded-xl text-white dark:text-black bg-[#4469A5] dark:bg-[#C3CFE2] font-bold p-2 text-xl hover:scale-[0.98] ease-in-out transition-all disabled:cursor-not-allowed disabled:opacity-80"
+                >
                   Login
                 </button>
-              </div>
+              </form>
             </div>
           </div>
         </motion.div>
